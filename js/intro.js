92cff3ab684ead5dc616435410e10e815657363c
Alan Ma <alan@Energymac.local>
2016-12-03 20:36:34 -0500

    intro page things

--- js/intro.js
+++ js/intro.js
@@ -2,17 +2,15 @@ var charNum = 0;
 var play_title = "";
 var play_synopsis = "";
 var characters = [];
-
 function submit() {
 	$('#enterInfo').submit(function(event) {
 		var $inputTitle = $(event.target).find('#inputTitle'); //get the name element from the input box
 		var title = $inputTitle.val(); //get the text from the name element
 		var $inputSynopsis = $(event.target).find('#inputSynopsis'); //get the direction element from the input box
 		var synopsis = $inputSynopsis.val(); //get the text from the direction element
-		$("#char").append("<h1>"+title+"</h1>");
-		$("#char").append("<p>"+synopsis+"</p>");
 		play_title = title;
 		play_synopsis = synopsis;
+		window.location.href = "index.html";
 		return false;
 	});
 
@@ -25,7 +23,6 @@ function submit() {
 		charNum += 1;
 		characters.push([charName, charDescription]);
 		return false;
-		window.location.href = "index.html";
 	});
 }
 
