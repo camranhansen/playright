3ff0daeec45dc81b36adb8e0426796ff40ba34a6
Alan Ma <alan@Energymac.local>
2016-12-03 20:15:46 -0500

    disregard this

--- js/pageT.js
+++ js/pageT.js
@@ -1,14 +1,14 @@
 var charNum = 0;
-var charList = [][];
+var charList = [];
 
 function submit() {
 	$('#enterInfo').submit(function(event) {
-		var $inputName = $(event.target).find('#inputTitle'); //get the name element from the input box
+		var $inputTitle = $(event.target).find('#inputTitle'); //get the name element from the input box
 		var title = $inputTitle.val(); //get the text from the name element
 		var $inputSynopsis = $(event.target).find('#inputSynopsis'); //get the direction element from the input box
 		var synopsis = $inputSynopsis.val(); //get the text from the direction element
 		$("#char").append("<h1>"+title+"</h1>");
-		$("#char").append("<p1>"+synopsis+"</p1>");
+		$("#char").append("<p>"+synopsis+"</p>");
 		return false;
 	});
 
@@ -18,12 +18,9 @@ function submit() {
 		var $inputCharName = $(event.target).find('#inputCharDescription');
 		var charDescription = $inputCharDescription.val();
 		$("#char").append("<h1>"+charName+"</h1>");
+		$("#char").append("<p>"+charDescription+"</p>");
 		charNum += 1;
 		
-		return false;
-	});
-	$('#enterChar').submit(function(event) {
-		$("#char").append("<p1>"+charDescription+"</p1>");
 		return false;
 	});
 }
