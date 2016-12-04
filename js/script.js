b7c09a7808f0683553e0e2390df5bcf82075c6d8
Alan Ma <alan@Energymac.local>
2016-12-03 19:03:20 -0500

    style update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var act = 0; //init act number
 var scene = 0; //init scene number
 var line = 0; //init line number
-var play = [];
+var play = ["!act!", "!scene!", ["Bob", "whisper", "It's alive!!!"]];
 function load() {
-
+	for(i=0; i<play.length; i++) {
+		if (play[i] == "!act!") {
+			addAct();
+		} else if (play[i] == "!scene!") {
+			addScene();
+		}
+		else {
+			addLine(play[i][0], play[i][1], play[i][2]);
+		}
+	}
 }
 function update() {
 	var objDiv = document.getElementById("script");
@@ -11,6 +20,51 @@ function update() {
 }
 function scrollTo(element) {
 	$("#script").scrollTop($("#script").scrollTop() + $("#"+element).position().top);
+}
+function addLine(name, direction, text) {
+	line += 1; //increment line number
+	var id = 'a'+act.toString()+'s'+scene.toString()+'l'+line.toString(); //id name
+	var directions = 0;
+	for (var i = 0, len = text.length; i < len; i++) {
+		if (text[i] == "#") {
+			directions += 1
+			if (directions % 2 == 1) {
+				text = [text.slice(0, i), "<i>(", text.slice(i+1)].join('');
+				len += 3;
+			} else {
+				text = [text.slice(0, i), ")</i>", text.slice(i+1)].join('');
+				len += 4;
+			}
+		}
+	}
+	if (name != "") {
+		$("#script").append('<table class="line" id="'+id+'"><tr></tr></table>'); //add line div
+		if (direction != "") {
+			$("#"+id).append('<td class="nameHolder"><span class="name">'+name+'</span> <span class="direction"> ('+direction+')</span></td>');
+		} else {
+			$("#"+id).append('<td class="nameHolder"><span class="name"'+name+'</span></td>');
+		}
+		$("#"+id).append('<td class="text">'+text+'</td>');
+	} else {
+		$("#script").append('<div class="stageDirection" id="'+id+'">'+text+'</div>');
+	}
+}
+function addAct() {
+	act += 1;
+	scene = 0;
+	line = 0;
+	var act_content = "Act "+act.toString();
+	var act_id = "act"+act.toString();
+	$("#script").append("<h1 class='act' id=\""+act_id+"\">"+act_content+"</h1>");
+	$("#toc").append('<button class="heading" onclick="scrollTo(\''+act_id+'\')">'+act_content+'</button>');
+}
+function addScene() {
+	scene += 1;
+	line = 0;
+	var scene_content = "Scene "+scene.toString();
+	var scene_id = "scene"+act.toString()+"-"+scene.toString();
+	$("#script").append("<h2 class='scene' id=\""+scene_id+"\">"+scene_content+"</h2>");
+	$("#toc").append('<button class="subheading" onclick="scrollTo(\''+scene_id+'\')">'+scene_content+'</button>');
 }
 function submit() {
 	$('#enterLine').submit(function(event) {
@@ -46,6 +100,7 @@ function submit() {
 		} else {
 			$("#script").append('<div class="stageDirection" id="'+id+'">'+text+'</div>');
 		}
+		play.push([name, direction, text]);
 		update();
 		return false;
 	});
@@ -58,6 +113,7 @@ function submit() {
 		$("#script").append("<h1 class='act' id=\""+act_id+"\">"+act_content+"</h1>");
 		$("#toc").append('<button class="heading" onclick="scrollTo(\''+act_id+'\')">'+act_content+'</button>');
 		update();
+		play.push("!act!");
 		return false;
 	});
 	$('#addScene').submit(function(event) {
@@ -68,10 +124,12 @@ function submit() {
 		$("#script").append("<h2 class='scene' id=\""+scene_id+"\">"+scene_content+"</h2>");
 		$("#toc").append('<button class="subheading" onclick="scrollTo(\''+scene_id+'\')">'+scene_content+'</button>');
 		update();
+		play.push("!scene!");
 		return false;
 	});
 }
 function main() {
+	load();
     submit();
     update();
 }
